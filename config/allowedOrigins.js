5d4ea2ec865eae255c341f5b95172c9133c899f0
martinnguyen-it <martin.nguyentam@gmail.com>
2023-10-05 10:41:16 +0700

    update cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ['https://express-js-demo-fe.vercel.app/'];
+const allowedOrigins = [
+    'https://express-js-demo-fe.vercel.app/',
+    'https://express-js-demo-fe.vercel.app',
+];
 
 module.exports = allowedOrigins;
